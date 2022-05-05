5edbc13c4fc6a1ad3ef41a4012e1adece48b728d
darksonok@yandex.ru <darksonok@yandex.ru>
2022-05-05 23:49:26 +0300

    Линтер ругался, поправил ошибки

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { render } from './render.js';
-import NewRoutePointCreatorView from './view/creating-form.js';
 import BoardPresenter from './presenter/trips-presenter.js';
 import NewFiltersView from './view/filters.js';
 
 const siteMainElement = document.querySelector('main');
 const tripEvents = siteMainElement.querySelector('.trip-events');
 const tripFiltersContainer = document.querySelector('.trip-controls__filters');
-//render(new NewRoutePointCreatorView(), body);
 render(new NewFiltersView, tripFiltersContainer);
 const boardPresenter = new BoardPresenter;
 
