29343e6bf0c4448d7868ff1b8adfd8d1bb5850ee
darksonok@yandex.ru <darksonok@yandex.ru>
2022-05-27 16:09:18 +0300

    Выполнение задание 5-1

--- src/mock/trip.js
+++ src/mock/trip.js
@@ -1,9 +1,11 @@
+import { nanoid } from 'nanoid';
+
 export const generateTrips = () => ({
   basePrice: 1100,
   dateFrom: '2019-07-10T22:55:56.845Z',
   dateTo: '2019-07-11T11:22:13.375Z',
   destination: 'LowerVasuki',
-  id: 0,
+  id: nanoid(),
   isFavorite: false,
   offers: [2, 3,6],
   type: 'taxi'
